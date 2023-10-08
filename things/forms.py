02902916fe7ac899a5156e853e3ba17ff9767f6a
Clara <clara.laoui@kcl.ac.uk>
2023-10-08 20:40:09 +0100

    try 2

--- things/forms.py
+++ things/forms.py
@@ -5,6 +5,7 @@ from .models import Thing
 
 class ThingForm(forms.ModelForm):
     class Meta:
-        model:Thing
-        fields:['name', 'description', 'quantity']
+        model = Thing
+        fields= ['name', 'description', 'quantity']
+        widgets = {'description' : forms.Textarea()}
 # Create your forms here.
--- things/models.py
+++ things/models.py
@@ -5,6 +5,6 @@ class Thing(models.Model):
     name = models.CharField(max_length=35, unique=True)
     description = models.TextField(max_length=120, blank=True)
     quantity = models.IntegerField(
-        validators=[MinValueValidator(0),MaxValueValidator(50)]
+        validators=[MinValueValidator(0), MaxValueValidator(50)]
     )
     created_at = models.DateTimeField(auto_now_add=True)
