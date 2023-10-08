b9fad51cb05f67f0b99ad84e4adaea4505b11646
Clara <clara.laoui@kcl.ac.uk>
2023-10-08 20:34:01 +0100

    try 1

--- things/forms.py
+++ things/forms.py
@@ -1,3 +1,10 @@
 """Forms of the project."""
+from django import forms 
+from .models import Thing
 
+
+class ThingForm(forms.ModelForm):
+    class Meta:
+        model:Thing
+        fields:['name', 'description', 'quantity']
 # Create your forms here.
--- things/models.py
+++ things/models.py
@@ -3,7 +3,7 @@ from django.db import models
 
 class Thing(models.Model):
     name = models.CharField(max_length=35, unique=True)
-    description = models.CharField(max_length=120, blank=True)
+    description = models.TextField(max_length=120, blank=True)
     quantity = models.IntegerField(
         validators=[MinValueValidator(0),MaxValueValidator(50)]
     )
